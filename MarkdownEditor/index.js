2e9b541788a2da2bf542411b5fad8715bb53a30a
Siddharth Pochampally <siddharthpochampally@gmail.com>
2020-11-01 20:54:01 -0800

    implemented code for plaintexteditor and markdowneditor

--- MarkdownEditor/index.js
+++ MarkdownEditor/index.js
@@ -1,15 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-
+import MarkdownIt from 'markdown-it';
+import dynamic from 'next/dynamic';
+import Head from 'next/head';
 import css from './style.css';
 
+//importing markdown editor
+const MdEditor = dynamic(() => import('react-markdown-editor-lite'), {
+  ssr: false
+});
+
+//initializing markdown parser for the editor
+const mdParser = new MarkdownIt({
+  html: true,
+  linkify: true,
+  typographer: true
+});
+
 function MarkdownEditor({ file, write }) {
-  console.log(file, write);
+  // console.log(file, write);
+  const [value, setValue] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    (async() => {
+      setValue(await file.text());
+      setIsLoading(false);
+    })();
+  }, [file]);
+
+  const handleEditorChange = ({ html, text }) => {
+    setValue(text);
+  }
+
+  const handleSave = () => {
+    const saveFile = new File([value], file.name, {
+      type: 'text/markdown',
+      lastModified: new Date()
+    });
+    write(saveFile);
+  }
+
   return (
-    <div className={css.editor}>
-      <h3>TODO</h3>
-      <i>text/markdown</i>
-    </div>
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/react-markdown-editor-lite/1.2.2/index.min.css"
+        />
+      </Head>
+      <div className={css.editor}>
+        {isLoading ? ( <h3>Loading the file</h3> ) : (
+          <MdEditor
+            value={value}
+            style={{ height: "350px "}}
+            renderHTML={(text) => mdParser.render(text)}
+            onChange={handleEditorChange}
+            onBlur={handleSave}
+          />
+        )}
+      </div>
+    </>
   );
 }
 
--- components/PlaintextEditor/index.js
+++ components/PlaintextEditor/index.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-
+import { TrixEditor } from 'react-trix';
 import css from './style.css';
 
 function PlaintextEditor({ file, write }) {
-  console.log(file, write);
+  // console.log(file, write);
+  const [value, setValue] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    (async () => {
+      setValue(await file.text());
+      setIsLoading(false);
+    })();
+  }, [file]);
+
+  const handleEditorReady = (editor) => {
+    editor.insertString(value);
+  };
+
+  const handleChange = (html, text) => {
+    setValue(text);
+  }
+
+  const handleSave = () => {
+    const saveFile = new File([value], file.name, {
+      type: 'text/plain',
+      lastModified: new Date()
+    });
+    write(saveFile);
+  }
+
   return (
     <div className={css.editor}>
-      <h3>TODO</h3>
-      <i>text/plain</i>
+      {isLoading ? ( <h2>Loading the file to view or edit</h2> ) : (
+        <>
+          <TrixEditor
+            onChange={handleChange}
+            onEditorReady={handleEditorReady}
+          />
+          <button type="submit" onClick={handleSave}>Save</button>
+        </>
+      )}
     </div>
   );
 }
